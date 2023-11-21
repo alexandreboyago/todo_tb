addc19f3998c742bc10aeb716e334672c31d3875
alexandreboyago <134931522+alexandreboyago@users.noreply.github.com>
2023-11-21 08:27:41 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import * as C from './App.styles';
+import { Item } from './types/Item';
+import { ListItem } from './components/ListItem/index';
+
+const App = () => {
+  
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name:'Ir até a padaria', done: false},
+    { id: 2, name:'Compar pão', done: true},
+  ]);
+  
+
+  return(
+    <C.Container>
+        <C.Area>
+          <C.Header>Lista de Tarefas</C.Header>
+
+          {/* Área para adicionar as tarefas */}
+          {
+            list.map( (item, index) => (
+              <ListItem key={index} item={item}/>
+            ) )
+          }
+
+        </C.Area>
+    </C.Container>
+  )
+}
+export default App;
